7e4849288da635f8c854ce48cc8484d098bcf0a8
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2020-03-30 16:50:51 +0500

    Updates

---
+++ precache-manifest.cafe834226e04e550fa2d15eadedad13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aeba7a431bd13461d9a5aac6b85b376",
+    "url": "/MOVIE-K/index.html"
+  },
+  {
+    "revision": "f783d6e4c164a5de769a",
+    "url": "/MOVIE-K/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "ef11d51609e266eade66",
+    "url": "/MOVIE-K/static/css/main.70a92ba2.chunk.css"
+  },
+  {
+    "revision": "f783d6e4c164a5de769a",
+    "url": "/MOVIE-K/static/js/2.846b9df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MOVIE-K/static/js/2.846b9df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef11d51609e266eade66",
+    "url": "/MOVIE-K/static/js/main.68eb3c46.chunk.js"
+  },
+  {
+    "revision": "339a762eabe643a93ff5",
+    "url": "/MOVIE-K/static/js/runtime-main.3a8feac2.js"
+  }
+]);
